49f6c340c0edb355f2c13e7005caecaf992274e3
manmaru97 <88545491+manmaru97@users.noreply.github.com>
2021-08-06 23:00:25 +0900

    Add files via upload

---
+++ 1_root/list.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// URLのリスト。無限に追加できる。ローカルの絶対パスにも対応（"\"ではなく"/"にする必要あり）
+let list = [
+    {
+        sitePass: 'https://www.youtube.com',
+        siteName: 'YouTube',
+        siteImage: 'kirby.gif'
+    },
+    {
+        sitePass: 'https://twitter.com/home',
+        siteName: 'Twitter',
+        siteImage: 'kirby.gif'
+    },
+    {
+        sitePass: 'https://www.amazon.co.jp',
+        siteName: 'amazon',
+        siteImage: 'kirby.gif'
+    },
+];
