02a5e8b34f6e65844b3a31e0d44a118c7619c06d
manmaru97 <88545491+manmaru97@users.noreply.github.com>
2022-03-06 14:45:19 +0900

    Update list.js

--- 1_root/list.js
+++ 1_root/list.js
@@ -3,18 +3,13 @@
 // URLのリスト。無限に追加できる。ローカルの絶対パスにも対応（"\"ではなく"/"にする必要あり）
 let list = [
     {
-        sitePass: 'https://www.youtube.com',
-        siteName: 'YouTube',
+        sitePass: 'https://manmaru97.github.io/game/game.html',
+        siteName: 'Game',
         siteImage: 'kirby.gif'
     },
     {
-        sitePass: 'https://twitter.com/home',
+        sitePass: 'https://twitter.com/manmaru97',
         siteName: 'Twitter',
-        siteImage: 'kirby.gif'
-    },
-    {
-        sitePass: 'https://www.amazon.co.jp',
-        siteName: 'amazon',
         siteImage: 'kirby.gif'
     },
 ];
